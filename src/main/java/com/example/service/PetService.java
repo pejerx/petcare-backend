f2af00e56b1d5148cb94e59d552600699710bcaa
LAB15_USER ON WS22 <L15X12W22@citulab.net>
2025-06-25 13:43:05 +0800

    Pet

---
+++ src/main/java/com/example/service/PetService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import com.example.model.Pet;
+import com.example.repository.PetRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PetService {
+
+    private final PetRepository repository;
+
+    public PetService(PetRepository repository) {
+        this.repository = repository;
+    }
+
+    public Pet create(Pet pet) {
+        return repository.save(pet);
+    }
+
+    public List<Pet> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Pet> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Pet update(Long petId, Pet newData) {
+        newData.setId(petId);
+        return repository.save(newData);
+    }
+
+    public void delete(Long petId) {
+        repository.deleteById(petId);
+    }
+}
